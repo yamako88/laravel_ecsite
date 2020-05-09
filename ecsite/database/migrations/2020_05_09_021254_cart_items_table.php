d9a7ba8c0f7808b54e5286928f408302fe264938
Yamauchi Ayaka <0810arare@gmail.com>
2020-05-09 11:30:37 +0900

    カートに商品を追加

---
+++ ecsite/database/migrations/2020_05_09_021254_cart_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('item_id');
+            $table->string('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
